be039f226e9b711d5bfa2b4881adc7a2453cee0c
min5ol <sol0508@kakao.com>
2025-07-13 11:18:33 +0900

    front - 홈 화면 구현 : 장민솔

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import routes from "./routes/router";
-import Header from "./components/templates/Header";
-import Footer from "./components/templates/Footer";
 
 function App()
 {
   return(
     <Router>
-      <Header />
       <Routes>
         {routes.map((route, idx) => (
           <Route key={idx} path={route.path} element={route.element} />
         ))}
       </Routes>
-      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/components/atoms/LoginSubmit.jsx
@@ -0,0 +1,6 @@
+export default function LoginSubmit(props)
+{
+  return(
+    <input type="submit" value={props.value} className="w-full h-[2.93vw] border-none rounded-full bg-primary text-w-ground text-[1.32vw] px-[1.46vw] font-ptd-400 outline-none" />
+  );
+}
