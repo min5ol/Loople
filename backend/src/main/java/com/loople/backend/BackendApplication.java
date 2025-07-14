65e0f4bb373517bb76bc054e81ee7c78d537a662
min5ol <sol0508@kakao.com>
2025-07-14 11:21:45 +0900

    v2  시작

--- backend/src/main/java/com/loople/backend/BackendApplication.java
+++ backend/src/main/java/com/loople/backend/BackendApplication.java
@@ -2,8 +2,21 @@ package com.loople.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(
+		basePackages = {
+				"com.loople.backend.v2"
+		},
+		excludeFilters = {
+				@ComponentScan.Filter(
+						type = FilterType.REGEX,
+						pattern = "com\\.loople\\.backend\\.api\\.v1\\..*"
+				)
+		}
+)
 public class BackendApplication {
 
 	public static void main(String[] args) {
